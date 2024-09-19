d9efc42d7c5db553016b0e8828a065dc468ad0f1
MotKyz <kuznetsov.matvey.2005@mail.ru>
2024-09-19 23:42:26 +0500

    Task01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,30 @@ public class Point {
     int x;
     int y;
 
+    Point(){
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    void flip(){
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
+
+    double distance(Point p) {
+        return Math.sqrt(Math.pow((p.x - x), 2) + Math.pow((p.y - y), 2));
     }
 }
